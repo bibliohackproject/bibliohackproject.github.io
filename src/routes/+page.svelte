36f9da994dc209d30726f92346efc10a6edd9431
SUGIMOTO Tatsuo <sugi2000@gmail.com>
2026-07-18 14:46:56 +0900

    feat: update project description and improve logo styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,20 @@
 <h1
   class="flex items-center gap-3 text-4xl px-4 py-12 bg-slate-600 text-slate-100"
 >
-  <Logo class="h-[1em] w-[1em]" />
+  <Logo class="h-10 w-10" />
   <span class="font-extralight">biblio<span class="font-semibold">hack</span> project</span>
 </h1>
 
 <div class="p-4">
   <div class="text-lg">
-    <span class="font-zalando-sans-expanded">bibliohack project</span>［ビブリオハック・プロジェクト］は、本を共有し、発見するためのプラットフォームを提供することを目指すプロジェクトです。読者、著者、出版社を結びつけ、本と読書を中心とした活気あるコミュニティを創り出すための共同の取り組みです。
+    <span class="font-zalando-sans-expanded">bibliohack project</span>［ビブリオハック・プロジェクト］は、
+    テクノロジーを活用し、本の情報を共有し、発見するさまざまなサービスを開発・提供することを目指すプロジェクトです。
+    読者、著者、出版社を結びつけ、本と読書を中心とした新しいコミュニティを創り出します。
   </div>
   <div class="text-lg">
-    <span class="font-zalando-sans-expanded">bibliohack project</span> is a project that aims to provide a platform for sharing
-    and discovering books. It is a collaborative effort that brings together
-    readers, authors, and publishers to create a vibrant community around books
-    and reading.
+    <span class="font-zalando-sans-expanded">bibliohack project</span>
+    ｓis a project that aims to develop and provide various services that leverage technology to share and discover book information. 
+    It brings together readers, authors, and publishers to create a new community centered around books and reading.
   </div>
 
   <h2 class="text-2xl mt-6">Projects</h2>
